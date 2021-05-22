ed84b0cfe8481b39881e0e2b2fd232b60ee34758
ruslan923 <ruslan.led@yandex.ru>
2021-05-22 18:10:42 +0300

    Second commit

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -1,38 +1,22 @@
 package ru.netology
 
 fun main() {
-    val costSong = 250
-    val countSong = 3
-    var buyPrevious = 200
+    val costSong = 300
+    val countSong = 5
+    val buyPrevious = 10001
     val buyNext = costSong * countSong
-    val buyWithDiscountMin = buyNext - 100
-    val buyWithDiscountMax = buyNext - (buyNext * 0.05)
-    val discountRegularCustomerMin = buyWithDiscountMin - (buyWithDiscountMin * 0.01)
-    val discountRegularCustomerMax = buyWithDiscountMax - (buyWithDiscountMax * 0.01)
     val regularCustomer = true
 
-
-    if (buyPrevious in 0..1000) {
-        println("Сумма вашей покупки составила $buyNext руб.")
-        print("На вашем счете $buyPrevious рублей")
-    } else {
-        if (buyPrevious in 1001..10_000 && regularCustomer) {
-            print(
-                "Сумма вашей покупки как постоянного покупателя $discountRegularCustomerMin руб.")
-        } else {
-            if (buyPrevious in 1001..10_000 && !regularCustomer) {
-                print("Сумма вашей покупки с учетом скидки $buyWithDiscountMin руб.")
-            } else {
-                if (buyPrevious >= 10_001 && regularCustomer) {
-                    print("Сумма вашей покупки как постоянного покупателя $discountRegularCustomerMax")
-                } else {
-                    if (buyPrevious >= 10_001 && !regularCustomer) {
-                        print("Сумма вашей покупки с учетом скидки $buyWithDiscountMax руб.")
-                    }
-                }
-            }
-        }
+    val amount = when (buyPrevious) {
+        in 0..1_000 -> buyNext
+        in 1_001..10_000 -> buyNext - 100
+        else -> buyNext * 0.95
     }
-    buyPrevious += buyNext
-    print("\nНа вашем счете $buyPrevious")
+
+    val totalAmount = if (regularCustomer) {
+        amount.toDouble() * 0.99
+    } else {
+        amount
+    }
+    println("Сумма вашей покупки составила - $totalAmount рублей")
 }
